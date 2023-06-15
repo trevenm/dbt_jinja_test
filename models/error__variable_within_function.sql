789168f671e07d0d904d5b2d73b4c7639a1ed599
Treven Mull <Treven.Mull@ouhealth.com>
2023-06-15 13:18:31 -0500

    Cleaned up for clarity

--- models/error__variable_within_function.sql
+++ models/error__variable_within_function.sql
@@ -1,21 +1,28 @@
-{{ config(enabled = false) }}
+{{ config(enabled = true) }}
 
-{% set date_query -%}
+{% set test_query -%}
     select 3 as num
 {% endset -%}
 
-{% set results = run_query(date_query) -%}
+{% set results = run_query(test_query) -%}
 
 {% if execute -%}
     {% set num = results[0][0] -%}
 {% endif -%}
 
-{% set test_var = num -%}
+{# Can't determine how to insert the variable within a set block. 
+    The following line fails with the error:
+    TypeError: 'Undefined' object cannot be interpreted as an integer -#}
 
-{# The following line fails with the error:
-   TypeError: 'Undefined' object cannot be interpreted as an integer -#}
+{# {% set test_var = range(1, num) -%} #}
 
-{# {% set test_var = range(1, num) -%} -#}  
+
+{# But this works fine -#}
+{% set test_var = range(1, 3) -%} 
+
+
+{# And so does this -#}
+{# {% set test_var = num -%} #}
 
 select 'test var = {{test_var}}' as test_column
 
