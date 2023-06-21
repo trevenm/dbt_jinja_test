407ecb590747d9f1faf7ad60f1adacb5eb989ab3
Treven Mull <Treven.Mull@ouhealth.com>
2023-06-21 09:53:20 -0500

    Revised error explanation

--- models/error__variable_within_function.sql
+++ models/error__variable_within_function.sql
@@ -1,29 +1,45 @@
-{{ config(enabled = true) }}
 
+{# Return an integer from a query and store as variable #}
 {% set test_query -%}
-    select 3 as num
+    select 2 as num2
 {% endset -%}
 
 {% set results = run_query(test_query) -%}
 
 {% if execute -%}
-    {% set num = results[0][0] -%}
+    {% set num2 = results[0][0] -%}
 {% endif -%}
 
-{# Can't determine how to insert the variable within a set block. 
-    The following line fails with the error:
-    TypeError: 'Undefined' object cannot be interpreted as an integer -#}
+{# select statement referencing variable will compile successfully #}
+select {{num2}} as num2   
 
-{# {% set test_var = range(1, num) -%} #}
+{# Can I use a variable as an argument in the range function?  YES #}
+{% set test_var = 2 -%}
+{% for i in range(1, test_var) -%}
+    select 'Test Select'
+{% endfor -%}
 
+{# Both variables are recognized as numbers #}
+{% if test_var is number -%}
+    select 'test_var is a number'
+    {% else -%}
+    select 'test_var failed number test'
+{% endif -%}
 
-{# But this works fine -#}
-{% set test_var = range(1, 3) -%} 
+{% if num2 is number -%}
+    select 'num2 is a number'
+    {% else -%}
+    select 'num2 failed number test'
+{% endif -%}
 
+{# ***************WHY**************** #}
+{# num2 variable used as argument, compile will fail #}
+{# TypeError: 'Undefined' object cannot be interpreted as an integer #}
 
-{# And so does this -#}
-{# {% set test_var = num -%} #}
+{# FAILS #}
+{% for i in range(1, num2) %}
+    select 'Test Select'
+{% endfor %}
 
-select 'test var = {{test_var}}' as test_column
 
 
